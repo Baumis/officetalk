d4d49db752dc3452514b0db02e93b3ea91ac4fb4
Baumis <baumgartneraxel@gmail.com>
2021-01-19 18:03:02 +0200

    first version login

--- src/Views/Login/Login.js
+++ src/Views/Login/Login.js
@@ -1,24 +1,48 @@
 import { useState } from 'react';
 import './Login.css';
 
-function Login() {
+function Login(props) {
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [loginType, setLoginType] = useState('user')
+
+    const isActive = (tab) => {
+        if (tab === 'user' && loginType === 'user'){
+            return 'user-active'
+        }
+        if (tab === 'organization' && loginType === 'organization') {
+            return 'organization-active'
+        }
+        return ''
+    }
+
+    const login = () => {
+        if (loginType === 'user') {
+            props.navigateTo('Office')
+        } else {
+            props.navigateTo('ControlPanel')
+        }
+    }
 
     return (
         <div className="login">
             <div className="login-title">
-                Welcome to OfficeTalk
-        </div>
+                Log in to OfficeTalk
+            </div>
             <div className="login-card">
-                <div className="login-card-title">
-                    Login
+                <div className="login-card-tabs">
+                    <div className={`login-card-tab ${isActive('user')}`} onClick={() => setLoginType('user')}>
+                        User
+                    </div>
+                    <div className={`login-card-tab ${isActive('organization')}`} onClick={() => setLoginType('organization')}>
+                        Organization
+                    </div>
                 </div>
                 <div className="login-input-row">
                     <div className="login-input-title">
                     </div>
                     <input
-                        placeholder={'Username'}
+                        placeholder={'Email'}
                         value={username}
                         onChange={(event) => setUsername(event.target.value)}
                     />
@@ -29,8 +53,14 @@ function Login() {
                     <input
                         placeholder={'Password'}
                         value={password}
+                        type={'password'}
                         onChange={(event) => setPassword(event.target.value)}
                     />
+                </div>
+                <div className="login-input-row">
+                    <div className="login-button" style={{background: loginType === 'organization' && '#F74040'}} onClick={() => login()}>
+                        Log in
+                    </div>
                 </div>
             </div>
         </div>
