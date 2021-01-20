6eacfb224f6e843d4397f83386e2ba74f808a50f
Baumis <baumgartneraxel@gmail.com>
2021-01-20 20:19:01 +0200

    mockdata

--- src/Stores/OfficeStore.js
+++ src/Stores/OfficeStore.js
@@ -6,6 +6,35 @@ class OfficeStore {
     },
     {
         name: 'Laniluona'
+    },
+    {
+        name: 'Työhuone'
+    }]
+
+    users = [{
+        name: 'Axel',
+        room: 0
+    },
+    {
+        name: 'Esko',
+        room: 1
+    },
+    {
+        name: 'Peter',
+        room: 2
+    }]
+
+    messages = [{
+        sender: 'Peter',
+        content: 'Tää on tosi kiva toimisto.'
+    },
+    {
+        name: 'Esko',
+        content: 'Eihän tää oo ees valmis :D'
+    },
+    {
+        name: 'Axel',
+        content: 'Moi Esko.'
     }]
 
     constructor(rootStore) {
