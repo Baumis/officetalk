3ae2de3515b4167b21d076374be444f4d199867f
Baumis <baumgartneraxel@gmail.com>
2021-02-04 21:19:44 +0200

    borderSize for own avatar

--- src/Views/Office/components/Avatar/Avatar.js
+++ src/Views/Office/components/Avatar/Avatar.js
@@ -32,7 +32,7 @@ const Avatar = observer((props) => {
             top: props.user.position.coordinates.y,
             left: props.user.position.coordinates.x,
             transition: `all ${props.user.transitionTime}s ease-in-out`,
-            border: `2px solid ${props.user.userId === userStore.user._id ? '#1CBF73' : 'black'}`,
+            border: `${props.user.userId === userStore.user._id ? '2' : '1'}px solid ${props.user.userId === userStore.user._id ? '#1CBF73' : 'black'}`,
             backgroundImage: `url(${getUserAvatar()})`
         }}>
             {getIcon()}
